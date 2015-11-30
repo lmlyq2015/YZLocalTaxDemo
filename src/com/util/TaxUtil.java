21b26475c8fb1d94e9d969595d2122f03c82e030
lium <ming.liu@nbhmit.com>
2015-11-30 14:58:56 +0800

    vo and util create

--- src/com/util/TaxUtil.java
+++ src/com/util/TaxUtil.java
@@ -2,4 +2,10 @@ package com.util;
 
 public class TaxUtil {
 
+	private final static String MESSAGE_ACCOUNT = "shihui.zhu@nbhmit.com";
+	
+	private final static String MESSAGE_PASSWORD = "123456";
+	
+	private static String CURRENT_USER;
+	
 }
---
+++ src/com/vos/NotificationVo.java
@@ -0,0 +1,62 @@
+package com.vos;
+
+public class NotificationVo {
+
+	private String taxNo;
+	private String taxName;
+	private String lawRep;
+	private String lawRepMob;
+	private String taxer;
+	private String taxerMob;
+	private String admin;
+	private String adminMob;
+	public String getTaxNo() {
+		return taxNo;
+	}
+	public void setTaxNo(String taxNo) {
+		this.taxNo = taxNo;
+	}
+	public String getTaxName() {
+		return taxName;
+	}
+	public void setTaxName(String taxName) {
+		this.taxName = taxName;
+	}
+	public String getLawRep() {
+		return lawRep;
+	}
+	public void setLawRep(String lawRep) {
+		this.lawRep = lawRep;
+	}
+	public String getLawRepMob() {
+		return lawRepMob;
+	}
+	public void setLawRepMob(String lawRepMob) {
+		this.lawRepMob = lawRepMob;
+	}
+	public String getTaxer() {
+		return taxer;
+	}
+	public void setTaxer(String taxer) {
+		this.taxer = taxer;
+	}
+	public String getTaxerMob() {
+		return taxerMob;
+	}
+	public void setTaxerMob(String taxerMob) {
+		this.taxerMob = taxerMob;
+	}
+	public String getAdmin() {
+		return admin;
+	}
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+	public String getAdminMob() {
+		return adminMob;
+	}
+	public void setAdminMob(String adminMob) {
+		this.adminMob = adminMob;
+	}
+	
+}
